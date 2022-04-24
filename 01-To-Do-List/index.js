015210192fda49c1498fb60dc3cea777efb8e8c6
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-04-24 09:57:22 +0700

    add to-do list practic

---
+++ 01-To-Do-List/index.js
@@ -0,0 +1,8 @@
+const hello = <h1>Hello World</h1>;
+const todo = (
+  <ul>
+    <li>Makan ayam goreng</li>
+    <li>Minum jus alpukat</li>
+  </ul>
+);
+ReactDOM.render(todo, document.querySelector("#root"));
